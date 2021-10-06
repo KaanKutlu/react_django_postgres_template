856aa008b7c6530e956f6fdf0857cb835cd3d7c6
dstreett <streett.david@gmail.com>
2021-10-06 11:42:34 -0600

    Updated to be tutorial react site

--- app/frontend/src/App.test.js
+++ app/frontend/src/App.test.js
@@ -1,27 +1,8 @@
+import { render, screen } from '@testing-library/react';
 import App from './App';
-import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
 
-let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
-
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-it("ensure header is in placae", () => {
-  act(() => {
-    render(<App data-testid="map" />, container);
-  });
-  
-
-  expect(container.textContent).toBe("Hello, World");
-})
